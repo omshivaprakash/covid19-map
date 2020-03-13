28a19edccfb67ca7a023b773eff4d06d275ad0a9
daniel-karl <daniel.karl@ibm.com>
2020-03-13 18:14:18 -0400

    initial import

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ import ReactTooltip from "react-tooltip";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/FormControl';
-import Button from 'react-bootstrap/Button';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserMd } from '@fortawesome/free-solid-svg-icons';
@@ -21,7 +16,7 @@ function App() {
         [
           <Navbar bg="light" expand="lg">
             <Navbar.Brand href="#home">
-              <FontAwesomeIcon icon={faUserMd} /> COVID19
+                <FontAwesomeIcon icon={faUserMd} /> <span className="small">COVID19</span>
             </Navbar.Brand>
           </Navbar>,
 
