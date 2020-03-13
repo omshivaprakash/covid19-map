23f5f0c1720f387e63b0aca6d26a5ad31b1985d3
daniel-karl <daniel.karl@ibm.com>
2020-03-13 18:50:47 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import MapChart from "./MapChart";
 import ReactTooltip from "react-tooltip";
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -9,27 +10,29 @@ import Nav from 'react-bootstrap/Nav';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserMd, faDatabase, faCode } from '@fortawesome/free-solid-svg-icons';
+import { faBookDead} from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   const [content, setContent] = useState("");
   return (
 
         [
-          <Navbar bg="light" fixed="top" className={"p-0 pl-2"}>
+          <Navbar bg="light" fixed="top" className={"p-0 pl-2"}  expand="lg">
             <Navbar.Brand>
                 <FontAwesomeIcon icon={faUserMd} />
                 <span className="small"> COVID19 </span>
             </Navbar.Brand>
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                  <Nav.Link className="small p-0 m-0 mr-3" href={"https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_time_series"}>
-                    <FontAwesomeIcon icon={faDatabase} /> Data
-                  </Nav.Link>
-                  <Nav.Link className="small p-0 m-0" href={"https://github.com/daniel-karl/covid19-map"}>
-                    <FontAwesomeIcon icon={faCode} /> Code
-                  </Nav.Link>
-                </Nav>
-              </Navbar.Collapse>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" className={"border-0 "} />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link className="small" href={"https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_time_series"}>
+                  <FontAwesomeIcon icon={faDatabase} /> Data source
+                </Nav.Link>
+                <Nav.Link className="small" href={"https://github.com/daniel-karl/covid19-map"}>
+                  <FontAwesomeIcon icon={faCode} /> Source code
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
           </Navbar>,
 
           <Container fluid className={"p-0"}>
