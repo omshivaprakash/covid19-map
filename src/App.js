5d8a0fb797b49c853f614c812207d841ba5e8571
daniel-karl <daniel.karl@ibm.com>
2020-03-14 00:29:17 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	README.md

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" className={"border-0 "} />
         <Navbar.Collapse id="basic-navbar-nav">
-            <span class="small text-danger">Is John Hopkins' COVID19 map accurate?</span>
+            <span class="small text-danger">How accurate is John Hopkins' COVID19 map?</span>
           <Nav className="mr-auto">
             <Nav.Link className="small" href={"https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_time_series"}>
               <FontAwesomeIcon icon={faDatabase} /> Data source
