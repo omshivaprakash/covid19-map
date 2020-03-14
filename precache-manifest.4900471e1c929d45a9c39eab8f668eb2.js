18c7703754ddaae9790e298b8bfd089b558f1a4a
daniel-karl <daniel.karl@ibm.com>
2020-03-14 05:04:14 -0400

    Updates

---
+++ precache-manifest.4900471e1c929d45a9c39eab8f668eb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5d5d2c1e8089f85705ac33862269b89",
+    "url": "/covid19-map/index.html"
+  },
+  {
+    "revision": "8a3ac96270b18c47c672",
+    "url": "/covid19-map/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "79ed1cce731e6ec91fee",
+    "url": "/covid19-map/static/css/main.2cbb4d93.chunk.css"
+  },
+  {
+    "revision": "8a3ac96270b18c47c672",
+    "url": "/covid19-map/static/js/2.0ed264e2.chunk.js"
+  },
+  {
+    "revision": "03d0a4cc290a81f82f2f98bdc220191c",
+    "url": "/covid19-map/static/js/2.0ed264e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79ed1cce731e6ec91fee",
+    "url": "/covid19-map/static/js/main.c2855fa1.chunk.js"
+  },
+  {
+    "revision": "ceae7fc2cae46046a9e7",
+    "url": "/covid19-map/static/js/runtime-main.a4c2c609.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid19-map/precache-manifest.ab225bc15ee6a30f617b725b443ba0e7.js"
+  "/covid19-map/precache-manifest.4900471e1c929d45a9c39eab8f668eb2.js"
 );
 
 self.addEventListener('message', (event) => {
