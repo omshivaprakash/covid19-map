fe0a771fe66aaff2568c7c068279d6f584d4708e
daniel-karl <daniel.karl@ibm.com>
2020-03-16 13:19:19 -0400

    downgraded weight of holy see

--- src/MapChart.js
+++ src/MapChart.js
@@ -288,7 +288,7 @@ let population = {
   "Guyana": 786559,
   "Haiti": 11402533,
   "High-income countries": 1263092934,
-  "Holy See": 809,
+  "Holy Sea": 809,
   "Honduras": 9904608,
   "Hubei, China": 58500000,
   "Hungary": 9660350,
@@ -822,10 +822,10 @@ class MapChart extends React.Component {
           <option value="last7">Show change last 7 days</option>
         </Form.Control>
         <ReactBootstrapSlider value={this.state.factor} change={e => {this.setState({ factor: e.target.value, width: e.target.value / 10 });}} step={1} max={100} min={1} />
-        <Form.Check inline className="small" checked={that.state.ppmmode} label={<span>Normalize by population</span>} type={"checkbox"} name={"a"} id={`inline-checkbox-3`}
+        <Form.Check inline className="small hideInJh" checked={that.state.ppmmode} label={<span>Normalize by population</span>} type={"checkbox"} name={"a"} id={`inline-checkbox-3`}
                     onClick={() => {that.setState({ppmmode: !that.state.ppmmode, chart: "pie", factor: 20, momentum: "none"});}} />
         <Form.Check inline className="small" checked={that.state.jhmode} label={<span>Johns Hopkins Mode </span>} type={"checkbox"} name={"a"} id={`inline-checkbox-2`}
-                    onClick={() => {that.setState({jhmode: !that.state.jhmode, chart: "pie", factor: 20, momentum: "none"});}} />
+                    onClick={() => {that.setState({jhmode: !that.state.jhmode, ppmmode: false, chart: "pie", factor: 20, momentum: "none"});}} />
       </div>
       {
         that.state.jhmode &&
