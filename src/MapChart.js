d0c2851bef93334599255476f094db464678f792
daniel-karl <daniel.karl@ibm.com>
2020-03-17 23:29:41 -0400

    windows collapsible

--- src/MapChart.js
+++ src/MapChart.js
@@ -8,7 +8,8 @@ import {
 } from "react-simple-maps";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faWindowMinimize, faWindowRestore } from '@fortawesome/free-solid-svg-icons';
+import { faWindowMinimize } from '@fortawesome/free-solid-svg-icons';
+import {faWindowRestore} from '@fortawesome/free-regular-svg-icons';
 
 import Papa from "papaparse";
 import Form from 'react-bootstrap/Form';
