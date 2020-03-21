a53d61ba7dfe34c93eec26ed32808cfada91f939
daniel-karl <daniel.karl@ibm.com>
2020-03-21 12:41:22 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid19-map/precache-manifest.ede0904fc6ed0e98221eede8f7d4fdb1.js"
+  "/covid19-map/precache-manifest.fa4b98603baab34a14e8f8a5be2813a8.js"
 );
 
 self.addEventListener('message', (event) => {
