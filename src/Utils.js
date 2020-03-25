304d5b76784e275af684829d72e2f9c7cffcc2de
daniel-karl <daniel.karl@ibm.com>
2020-03-25 09:44:46 -0400

    nicer tooltip

---
+++ src/Utils.js
@@ -0,0 +1,15 @@
+
+export default class Utils {
+
+    static rounded = num => {
+        if (num > 1000000000) {
+            return Math.round(num / 100000000) / 10 + "Bn";
+        } else if (num > 1000000) {
+            return Math.round(num / 100000) / 10 + "M";
+        } else if (num > 1000) {
+            return Math.round(num / 100) / 10 + "K";
+        } else {
+            return Math.round(num);
+        }
+    };
+}
